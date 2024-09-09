19d6a6be97316f914cf5fe520d4e5fca1fae4950
mona <mona-alrayes>
2024-09-09 21:35:41 +0300

    'update routes doc'

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -11,34 +10,38 @@ use App\Http\Controllers\UserController;
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| This file defines all the API routes for the application. The routes are
+| grouped based on their functionality and assigned appropriate middleware,
+| such as 'auth:api' and role-based access control.
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
+
+// Auth routes for login, register, logout, and token refresh
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('register', 'register');
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
-
 });
 
-Route::group(['middleware' => [ 'auth:api','role:admin']], function () {
-    Route::apiResource('users', UserController::class)->only('update');
-    Route::apiResource('tasks', TaskController::class)->except('updateByAssignedUser');
+// Admin-only routes
+Route::group(['middleware' => ['auth:api', 'role:admin']], function () {
+    Route::apiResource('users', UserController::class)->only(['update']);
+    Route::apiResource('tasks', TaskController::class)->except(['updateByAssignedUser']);
 });
 
-Route::group(['middleware' => [ 'auth:api','role:user']], function () {
+// User-only routes for task status change
+Route::group(['middleware' => ['auth:api', 'role:user']], function () {
     Route::put('/tasks/{id}/changeStatus', [TaskController::class, 'updateByAssignedUser']);
 });
-Route::group(['middleware' => [ 'auth:api','role:manager']], function () {
+
+// Manager-only routes for task assignment
+Route::group(['middleware' => ['auth:api', 'role:manager']], function () {
     Route::put('/tasks/{id}/assign', [TaskController::class, 'update']);
 });
-Route::apiResource('users', UserController::class)->except('update');
+
+// Publicly accessible routes to view users and tasks
+Route::apiResource('users', UserController::class)->except(['update']);
 Route::apiResource('tasks', TaskController::class)->only(['index', 'show']);
